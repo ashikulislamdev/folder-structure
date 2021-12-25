09251cc3706e400716169c8165c0347ef885fdf4
Ahikul Islam Saun <66787620+ashikulislamdev@users.noreply.github.com>
2021-12-25 19:09:32 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>iSchool Project</title>
+	<!-- Bootstrap css file -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Fontawesome css file -->
+	<link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+	<!-- Custom Css -->
+	<link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+
+</head>
+<body>
+	<!-- Start Nav bar -->
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+	  <a class="navbar-brand" href="#">iSchool</a>
+	  <span class="navbar-text">Learn and Impliment</span>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+	    <ul class="navbar-nav custom-nav">
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="index.php">Home </a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Course</a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Payment</a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Login</a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Sign-up</a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Feedback</a>
+	      </li>
+	      <li class="nav-item custom-nav-item">
+	        <a class="nav-link" href="#">Contact</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	<!-- End nav bar -->
+
+	<!-- Neccessary js file -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/fontawesome-all.min.js"></script>
+</body>
+</html>
